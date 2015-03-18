15ffcb0d6c4ad51e7bc9a851220ed13ace39fd90
asha-games <ashaforgames@gmail.com>
2015-03-18 15:13:44 +1030

    change to windowed mode

--- src/com/gihs/Game.java
+++ src/com/gihs/Game.java
@@ -5,14 +5,19 @@ import processing.core.*;
 public class Game extends PApplet {
 
     public void setup() {
-        size(200,200);
+        size(200, 200);
+        if (frame != null) {
+            frame.setResizable(true);
+        }
         background(0);
     }
 
     public void draw() {
         stroke(255);
-        if (mousePressed) {
-            line(mouseX,mouseY,pmouseX,pmouseY);
+        if (mousePressed && (mouseButton == LEFT)) {
+            line(mouseX, mouseY, pmouseX, pmouseY);
+        } else if (mousePressed && (mouseButton == RIGHT)) {
+            background(0);
         }
     }
 }
--- src/com/gihs/Main.java
+++ src/com/gihs/Main.java
@@ -5,6 +5,6 @@ import processing.core.PApplet;
 public class Main {
 
     public static void main(String args[]) {
-        PApplet.main(new String[]{"--present", "com.gihs.Game"});
+        PApplet.main("com.gihs.Game");
     }
 }
